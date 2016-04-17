a38fdd7ff663ed56f4829dc20d3172e40e545eb8
Alco_Rs11 <RIGHTNESSB@GMAIL.COM>
2016-04-17 00:12:55 -0400

    Adds more ranks to show in HTTP.
    Allows for optional forums implementation.

--- src/me/StevenLawson/TotalFreedomMod/HTTPD/Module_players.java
+++ src/me/StevenLawson/TotalFreedomMod/HTTPD/Module_players.java
@@ -26,6 +26,13 @@ public class Module_players extends TFM_HTTPD_Module
         final JSONArray telnetadmins = new JSONArray();
         final JSONArray senioradmins = new JSONArray();
         final JSONArray developers = new JSONArray();
+        final JSONArray executives = new JSONArray();
+        final JSONArray specialists = new JSONArray();
+        final JSONArray systemadmins = new JSONArray();
+        final JSONArray leadsystemadmins = new JSONArray();
+        final JSONArray coowners = new JSONArray();
+        final JSONArray owners = new JSONArray();
+
 
         // All online players
         for (Player player : TotalFreedomMod.server.getOnlinePlayers())
@@ -67,12 +74,29 @@ public class Module_players extends TFM_HTTPD_Module
 
         // Developers
         developers.addAll(TFM_Util.DEVELOPERS);
-
+        // Executives
+        executives.addAll(TFM_Util.EXECS);
+        // Specialists
+        specialists.addAll(TFM_Util.SPECIAL_EXECS);
+        // System-Admins
+        systemadmins.addAll(TFM_Util.SYS_ADMINS);
+        // Lead System-Admins
+        leadsystemadmins.addAll(TFM_Util.LEADSYSS);
+        // Co-Owners
+        coowners.addAll(TFM_Util.SYS_COOWNERS);
+        // Owners
+        owners.addAll(TFM_Util.OWNERS);
         responseObject.put("players", players);
         responseObject.put("superadmins", superadmins);
         responseObject.put("telnetadmins", telnetadmins);
         responseObject.put("senioradmins", senioradmins);
         responseObject.put("developers", developers);
+        responseObject.put("executives", executives);
+        responseObject.put("specialists", specialists);
+        responseObject.put("system-admins", systemadmins);
+        responseObject.put("lead system-admins", leadsystemadmins);
+        responseObject.put("co-owners", coowners);
+         responseObject.put("owners", owners);
 
         final NanoHTTPD.Response response = new NanoHTTPD.Response(NanoHTTPD.Response.Status.OK, NanoHTTPD.MIME_JSON, responseObject.toString());
         response.addHeader("Access-Control-Allow-Origin", "*");
